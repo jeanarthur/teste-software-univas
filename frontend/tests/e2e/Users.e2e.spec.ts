eaa7afe620df1590ca96e5a71e81715f341ffc1a
jeanarthur <jean.arthursa@gmail.com>
2025-11-25 20:10:10 -0300

    feat: adiciona testes e2e - atualizar e excluir usuario

--- frontend/tests/e2e/Users.e2e.spec.ts
+++ frontend/tests/e2e/Users.e2e.spec.ts
@@ -22,4 +22,34 @@ test.describe('Usuários', () => {
         await expect(page.getByText(uniqueEmail)).toBeVisible()
     });
 
+    test('atualiza usuário e aparece na lista', async ({ page }) => {
+        await page.goto('/users')
+        await page.getByRole('button', { name: /Editar/i }).first().click()
+        const uniqueEmail = `aluno.${Date.now()}.atualizado@ex.com`
+        await page.getByLabel('Nome:').fill('Aluno E2E (Atualizado)')
+        await page.getByLabel('Email:').fill(uniqueEmail)
+        await page.getByRole('button', { name: /Atualizar/i }).click()
+        // Aguarda recarga da lista
+        await expect(page.getByText(uniqueEmail)).toBeVisible()
+    });
+
+    test('exluir usuário e aparece na lista', async ({ page }) => {
+        await page.goto('/users')
+
+        // cria um usuário
+        await page.getByRole('button', { name: /Adicionar Usuário/i }).click()
+        const uniqueEmail = `aluno.${Date.now()}@ex.com`
+        await page.getByLabel('Nome:').fill('Aluno E2E')
+        await page.getByLabel('Email:').fill(uniqueEmail)
+        await page.getByRole('button', { name: /Criar/i }).click()
+        // Aguarda recarga da lista
+        await expect(page.getByText(uniqueEmail)).toBeVisible()
+
+        // exclui o usuário criado
+        page.on('dialog', dialog => dialog.accept())
+        await page.getByRole('button', { name: /Excluir/i }).first().click()
+        // Aguarda recarga da lista
+        await expect(page.getByText(uniqueEmail)).not.toBeVisible()
+    });
+
 });
